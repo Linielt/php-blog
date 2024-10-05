37ca32c5f9b86b3dda9cd3a16f0e599f03739f85
Linielt <danielhlee912@gmail.com>
2024-10-05 11:19:08 +0100

    Created initial homepage for blog

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>A Basic Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+<body>
+    <header>
+        <h1>A Basic Blog</h1>
+        <p>A summary paragraph about what this blog is about.</p>
+    </header>
+
+    <main>
+        <article>
+            <h2>Article 1 Title</h2>
+            <div>dd MON YYYY</div>
+            <p>A summary paragraph about Article 1.</p>
+            <p>
+                <a href="#">Read more...</a>
+            </p>
+        </article>
+
+        <article>
+            <h2>Article 2 Title</h2>
+            <div>dd MON YYYY</div>
+            <p>A summary paragraph about Article 2.</p>
+            <p>
+                <a href="#">Read more...</a>
+            </p>
+        </article>
+    </main>
+</body>
+</html>
